dd1bf848a57518e2005bd949db1ba038fe7e2026
consarnproject <132343102+consarnproject@users.noreply.github.com>
2025-07-07 12:56:00 +0200

    Add files via upload

--- format.sh
+++ format.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 clang-format --version
-find lib orm_lib nosql_lib examples drogon_ctl -name *.h -o -name *.cc -exec dos2unix {} \;
-find lib orm_lib nosql_lib examples drogon_ctl -name *.h -o -name *.cc|xargs clang-format -i -style=file
+
+find trantor -name *.h -o -name *.cc -exec dos2unix {} \;
+find trantor -name *.h -o -name *.cc|xargs clang-format -i -style=file
